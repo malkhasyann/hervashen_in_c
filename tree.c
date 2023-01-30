d12a62c622c237059f0c2c749006500679389012
malkhasyan <ara.malkhasyan2019@gmail.com>
2023-01-31 01:28:32 +0400

    implemented tree

---
+++ tree.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// tree: type definition
+typedef struct
+{
+    int* pos;
+} tree_t;
+
+// tree: memory allocation
+tree_t* tree_malloc()
+{
+    return (tree_t*)malloc(sizeof(tree_t));
+}
+
+// tree: constructor
+void tree_init(tree_t* this, int* pos)
+{
+    this->pos = (int*)malloc(2 * sizeof(int));
+    this->pos[0] = pos[0];
+    this->pos[1] = pos[1];
+}
+
+// tree: destructor
+void tree_destroy(tree_t* this)
+{
+    free(this->pos);
+}
+
+// tree: getter(pos)
+int* tree_get_pos(tree_t* this)
+{
+    return this->pos;
+}
+
+// tree: photosynthesis
+int tree_photosynthesis(tree_t* this)
+{
+    return 80;
+}
+
+// tree: display info
+void tree_display(tree_t* this)
+{
+    printf("Tree::position: {%d, %d}\n",
+            this->pos[0], this->pos[1]);
+}
---
+++ tree.h
@@ -0,0 +1,16 @@
+#ifndef TREE
+#define TREE
+
+struct tree_t;
+
+struct tree_t* tree_malloc();
+
+void tree_init(struct tree_t*, int[]);
+void tree_destroy(struct tree_t*);
+
+int* tree_get_pos(struct tree_t*);
+int tree_photosynthesis(struct tree_t*);
+
+void tree_display(struct tree_t*);
+
+#endif
